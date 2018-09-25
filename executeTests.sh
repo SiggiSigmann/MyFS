35308a7780fc4c824334a5d84dcf281f059bf1f2
Henning Häcker <Henning.Haecker@web.de>
2018-09-26 01:31:43 +0200

    Added Volume to enable Caching to reduce the compile time tremendously

--- executeTests.sh
+++ executeTests.sh
@@ -1,4 +1,11 @@
 #/bin/sh
+#build the image silently
 docker build -q -t test-machine . > /dev/null
-docker run --name test-container test-machine
+
+#run the container
+#using the 'obj' dir as volume enables the caching of linking objects to save compiling time
+#on docker for windows the volume 'obj' is located on the disk of the virtual machine (linux) hosting the docker environment and will persist forever by default
+docker run --name test-container -v obj:/MyFS/obj test-machine
+
+#remove the dead container silently
 docker rm test-container > /dev/null
