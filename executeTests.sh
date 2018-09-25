e9feb2499e60cdcc5e440cd097ec0d92e96fd006
Henning Häcker <Henning.Haecker@web.de>
2018-09-26 00:28:06 +0200

    Added silent deletion of dead container after a run, silenced building output

--- executeTests.sh
+++ executeTests.sh
@@ -1,3 +1,4 @@
 #/bin/sh
-docker build -q -t test-machine .
-docker run test-machine
+docker build -q -t test-machine . > /dev/null
+docker run --name test-container test-machine
+docker rm test-container > /dev/null
