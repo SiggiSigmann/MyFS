e9837aae4d6cdfc93d3aeec9400c92257ab5cd31
Tobias Sigmann <tobias.sigmann@googlemail.com>
2018-10-06 17:23:53 +0200

    create constants.h and superblock.h

---
+++ includes/constants.h
@@ -0,0 +1,13 @@
+/*
+Defines FS constants
+*/
+
+#ifndef const
+#define const
+
+#define NAME_LENGTH 255 
+#define BLOCK_SIZE 512
+#define NUM_DIR_ENTRIES 64          //maximum amount of files
+#define NUM_OPEN_FILES 64           //maximum amount of open files
+
+#endif
---
+++ includes/superblock.h
@@ -0,0 +1,26 @@
+/*
+Manage the superblock of the FS
+*/
+
+#ifndef superblock
+#define superblock
+
+struct SuperStruct{
+    uint dmapAddress;
+    uint fatAdress;
+    uint rootAdress;
+    uint maxFiles;
+    uint blockSize;
+    uint amountOfBlocks;
+}
+
+class SuperBlock{
+    private:
+        SuperStruct *superBlock;
+    public:
+
+
+}
+
+
+#endif
