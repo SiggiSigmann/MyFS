2735ecb4970d80edd30d0b203523d655cac2ba51
jufabeck2202 <ju-fa-beck@t-online.de>
2018-10-08 16:30:14 +0200

    added test case

--- unittests/test-inode.cpp
+++ unittests/test-inode.cpp
@@ -5,30 +5,38 @@
 
 
 #include "catch.hpp"
+#include <iostream>
 
 #include <stdio.h>
 #include <string.h>
 
 #include "helper.hpp"
+#include "inode.h"
 
 #include "blockdevice.h"
-#include "inode.h"
 #include "myfs.h"
 
-#define BD_PATH "/tmp/bd.bin"
+#define BD_PATH "/tmp/inode.bin"
 
 TEST_CASE( "Test Inode helper", "[inode]" ) {
-    
+    BlockDevice *bd = new BlockDevice(BLOCK_SIZE);
+
     remove(BD_PATH);
+    bd->create(BD_PATH);
     
-    BlockDevice bd;
-    bd.create(BD_PATH);
-    
-    SECTION("writing To inode") {
+    SECTION("write / read To inode") {
+        Inode *inodeHelper = new Inode();
+
+        int index = 10;
+        int testNumber = 1234;
+        inodeHelper->create(bd,index,testNumber);
         
+        InodeStruct* currentInode = (InodeStruct *)malloc(BLOCK_SIZE);
+        currentInode = inodeHelper->get(bd,index);
+        REQUIRE(testNumber==currentInode->dmapAddress);
     }
     
-    bd.close();
+    bd->close();
     remove(BD_PATH);
     
 }
