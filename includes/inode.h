9a0b8d4165781c883574667ab235d090da860da7
Julian Beck <ju-fa-beck@t-online.de>
2018-10-11 09:57:09 +0200

    added struct values to create function, removed used parameter

--- includes/inode.h
+++ includes/inode.h
@@ -24,7 +24,7 @@ struct InodeStruct{
 
 class Inode{
     public:  
-        void create(BlockDevice *bd, uint32_t index, char* fileName,uint32_t fileSize, uint32_t atime, uint32_t mtime, uint32_t ctime, uint32_t userID, uint32_t groupID, uint32_t mode);
+        void create(BlockDevice *bd, uint32_t index, char* fileName, uint32_t firstDataBlock, uint32_t fileSize, uint32_t atime, uint32_t mtime, uint32_t ctime, uint32_t userID, uint32_t groupID, uint32_t mode);
         InodeStruct* get(BlockDevice *bd, uint32_t index);
 
 };
--- src/inode.cpp
+++ src/inode.cpp
@@ -4,9 +4,11 @@
 #include <string.h>
 #include <iostream>
 
-void Inode::create(BlockDevice *bd, uint32_t index, char* fileName,uint32_t fileSize, uint32_t atime, uint32_t mtime, uint32_t ctime, uint32_t userID, uint32_t groupID, uint32_t mode){
+void Inode::create(BlockDevice *bd, uint32_t index, char* fileName, uint32_t firstDataBlock, uint32_t fileSize, uint32_t atime, uint32_t mtime, uint32_t ctime, uint32_t userID, uint32_t groupID, uint32_t mode){
     InodeStruct *newInode = (InodeStruct *)malloc(BLOCK_SIZE);
     newInode->fileName = fileName;
+    newInode->uses = false;
+    newInode->firstDataBlock = firstDataBlock
     newInode->fileSize = fileSize;
     newInode->atime = atime;
     newInode->mtime = mtime;
