6304d6b16a28a43a2d1a3b14be91f5af9abe94f3
jufabeck2202 <ju-fa-beck@t-online.de>
2018-10-11 18:50:55 +0200

    renamed functions

--- src/rootblock.cpp
+++ src/rootblock.cpp
@@ -4,7 +4,7 @@
 #include <string.h>
 #include <iostream>
 
-void Inode::create(BlockDevice *bd, uint32_t index, char* fileName,uint32_t fileSize, uint32_t atime, uint32_t mtime, uint32_t ctime, uint32_t userID, uint32_t groupID, uint32_t mode){
+void RootBlock::createInode(BlockDevice *bd, uint32_t index, char* fileName,uint32_t fileSize, uint32_t atime, uint32_t mtime, uint32_t ctime, uint32_t userID, uint32_t groupID, uint32_t mode){
     InodeStruct *newInode = (InodeStruct *)malloc(BLOCK_SIZE);
     newInode->fileName = fileName;
     newInode->fileSize = fileSize;
@@ -20,7 +20,7 @@ void Inode::create(BlockDevice *bd, uint32_t index, char* fileName,uint32_t file
 
 }
 
-InodeStruct* Inode::get(BlockDevice *bd, uint32_t index){
+InodeStruct* RootBlock::getInode(BlockDevice *bd, uint32_t index){
     //create struct the with the size of Block_Size
     InodeStruct* currentInode = (InodeStruct *)malloc(BLOCK_SIZE);
     bd->read(index, (char *)currentInode);
--- unittests/test-inode.cpp
+++ unittests/test-inode.cpp
@@ -11,7 +11,7 @@
 #include <string.h>
 
 #include "helper.hpp"
-#include "inode.h"
+#include "rootblock.h"
 
 #include "blockdevice.h"
 #include "myfs.h"
@@ -25,17 +25,17 @@ TEST_CASE( "Test Inode helper", "[inode]" ) {
     bd->create(BD_PATH);
     
     SECTION("write / read To inode") {
-        Inode *inodeHelper = new Inode();
+        RootBlock *inodeHelper = new RootBlock();
 
         uint32_t index = 10;
         //write inode with data to block 10
         char fileName[FILENAME_MAX] = "test";
-        inodeHelper->create(bd,index,fileName,100,100,100,100,100,100,100);
+        inodeHelper->createInode(bd,index,fileName,100,100,100,100,100,100,100);
         
         //create new inode struct to store data, size = blockSize
         InodeStruct* currentInode = (InodeStruct *)malloc(BLOCK_SIZE);
         //read inode struct from block 10
-        currentInode = inodeHelper->get(bd,index);
+        currentInode = inodeHelper->getInode(bd,index);
 
         REQUIRE(strcmp(fileName,currentInode->fileName) == 0);
         REQUIRE(100==currentInode->fileSize);
