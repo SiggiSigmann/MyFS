049cb410139cf9e59f8a944fdd6ea3fa000b1a28
Julian Beck <ju-fa-beck@t-online.de>
2018-10-12 08:16:19 +0200

    test create Rootblock

--- unittests/test-inode.cpp
+++ unittests/test-inode.cpp
@@ -27,22 +27,34 @@ TEST_CASE( "Test Inode helper", "[inode]" ) {
     SECTION("write / read To inode") {
         RootBlock *rootb = new RootBlock();
 
-        uint32_t index = 10;
+        uint32_t index = 1000;
         //write inode with data to block 10
         char fileName[FILENAME_MAX] = "test";
         rootb->createInode(bd,index,true,fileName,1,100,100,100,100,100,100,100);
         
         //create new inode struct to store data, size = blockSize
-        InodeStruct* currentInode = (InodeStruct *)malloc(BLOCK_SIZE);
+        InodeStruct* testInode = (InodeStruct *)malloc(BLOCK_SIZE);
         //read inode struct from block 10
-        currentInode = rootb->getInode(bd,index);
+        testInode = rootb->getInode(bd,index);
 
-        REQUIRE(strcmp(fileName,currentInode->fileName) == 0);
-        REQUIRE(100==currentInode->fileSize);
-        REQUIRE(100==currentInode->mode);
+        REQUIRE(strcmp(fileName,testInode->fileName) == 0);
+        REQUIRE(100==testInode->fileSize);
+        REQUIRE(100==testInode->mode);
+        //create rootblock with 64 inodes
         rootb->create(bd);
+        //get first inode
+        InodeStruct* testInode2 = (InodeStruct *)malloc(BLOCK_SIZE);
+        testInode = rootb->getInode(bd,I_MAP_FIRST_BLOCK);
+        //test first inode in rootblock
+        REQUIRE(false==testInode->used);
+        REQUIRE(strcmp(testInode->fileName,"empty") == 0);
+        print
+        
+
         delete rootb;
-        free(currentInode);
+        free(testInode);
+        free(testInode2);
+
 
     }
     
