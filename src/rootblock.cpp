86d4ae9e2984f604fe2b0f8fda313fcae73dcbdd
Julian Beck <ju-fa-beck@t-online.de>
2018-10-12 09:16:22 +0200

    added documents

--- src/rootblock.cpp
+++ src/rootblock.cpp
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <iostream>
 
+//create new Inode
 void RootBlock::createInode(BlockDevice *bd, uint32_t index, bool used, char* fileName, uint32_t firstDataBlock, uint32_t fileSize, uint32_t atime, uint32_t mtime, uint32_t ctime, uint32_t userID, uint32_t groupID, uint32_t mode){
     InodeStruct *newInode = (InodeStruct *)malloc(BLOCK_SIZE);
     newInode->used = false;
@@ -20,16 +21,15 @@ void RootBlock::createInode(BlockDevice *bd, uint32_t index, bool used, char* fi
     free(newInode);
 
 }
-
+//gets inode from blockdevice
 InodeStruct* RootBlock::getInode(BlockDevice *bd, uint32_t index){
     //create struct the with the size of Block_Size
     InodeStruct* currentInode = (InodeStruct *)malloc(BLOCK_SIZE);
     bd->read(index, (char *)currentInode);
     return currentInode;
 }
-
+//create rootblock, creates NUM_DIR_ENTRIES Inodes starting from I_MAP_FIRST_BLOCKS
 void RootBlock::create(BlockDevice*bd){
-
     //create inodes for rootblock
     for(int i;i <= NUM_DIR_ENTRIES; i++){
         
