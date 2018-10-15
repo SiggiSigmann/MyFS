87754f91c3ac649cb8d7bc923f378b970d041805
Julian Beck <ju-fa-beck@t-online.de>
2018-10-15 16:00:24 +0200

    changed index to blockIndex

--- includes/rootblock.h
+++ includes/rootblock.h
@@ -19,9 +19,9 @@ struct InodeStruct{
 
 class RootBlock{
     public:  
-        void createInode(BlockDevice *bd, uint32_t index, char *fileName, uint32_t firstDataBlock, uint32_t fileSize,
+        void createInode(BlockDevice *bd, uint32_t blockIndex, char *fileName, uint32_t firstDataBlock, uint32_t fileSize,
             uint32_t atime, uint32_t mtime, uint32_t ctime, uint32_t userID, uint32_t groupID, uint32_t mode);
-        InodeStruct* getInode(BlockDevice *bd, uint32_t index);
+        InodeStruct* getInode(BlockDevice *bd, uint32_t blockIndex);
         void init(BlockDevice*bd);
 };
 
--- src/rootblock.cpp
+++ src/rootblock.cpp
@@ -5,7 +5,7 @@
 #include <iostream>
 
 //create a new Inode directly on the block device
-void RootBlock::createInode(BlockDevice *bd, uint32_t index, char *fileName, uint32_t firstDataBlock, uint32_t fileSize,
+void RootBlock::createInode(BlockDevice *bd, uint32_t blockIndex, char *fileName, uint32_t firstDataBlock, uint32_t fileSize,
         uint32_t atime, uint32_t mtime, uint32_t ctime, uint32_t userID, uint32_t groupID, uint32_t mode){
     InodeStruct *inode = (InodeStruct *)malloc(BLOCK_SIZE);
     inode->firstDataBlock = firstDataBlock;
@@ -20,16 +20,16 @@ void RootBlock::createInode(BlockDevice *bd, uint32_t index, char *fileName, uin
     inode->groupID = groupID;
     inode->mode = mode;
     //write inode to block device
-    bd->write(index, (char *)inode);
+    bd->write(blockIndex, (char *)inode);
     //delete the inode in memory
     free(inode);
 }
 
 //reads and returns the demanded inode from the blockdevice
-InodeStruct* RootBlock::getInode(BlockDevice *bd, uint32_t index){
+InodeStruct* RootBlock::getInode(BlockDevice *bd, uint32_t blockIndex){
     //create struct with the size of BLOCK_SIZE
     InodeStruct* currentInode = (InodeStruct *)malloc(BLOCK_SIZE);
-    bd->read(index, (char *)currentInode);
+    bd->read(blockIndex, (char *)currentInode);
     return currentInode;
 }
 
