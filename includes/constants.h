5e6923e1e673114d3cf539596a5b9c27f4be4df4
Henning Häcker <Henning.Haecker@web.de>
2018-10-16 16:00:46 +0200

    robustated macros to be compliant with non-multiples of BD_BLOCK_SIZE

--- includes/constants.h
+++ includes/constants.h
@@ -12,13 +12,12 @@
 
 //general specs
 #define SUPERBLOCK_BLOCK_INDEX (1)
-//important: NUMBER_OF_USABLE_DATABLOCKS needs to be a multiple of BD_BLOCK_SIZE, to become perfectly mappable
-#define NUMBER_OF_USABLE_DATABLOCKS (1 << 18) //2^18 = 262144 blocks => 128 MiB in total
+#define NUMBER_OF_USABLE_DATABLOCKS (1 << 18)//2^18 = 262144 blocks => 128 MiB in total
 #define NUMBER_OF_INODES (NUM_DIR_ENTRIES)
 
 //I-Map
 #if ((NUMBER_OF_INODES%BD_BLOCK_SIZE) != 0)
-#define NUMBER_OF_I_MAP_BLOCKS ((NUMBER_OF_INODES/BD_BLOCK_SIZE)+1) //one more block than else case to store non-perfect-mappable inodes (inode numbers that differ multiples of BD_BLOCK_SIZE)
+#define NUMBER_OF_I_MAP_BLOCKS ((NUMBER_OF_INODES/BD_BLOCK_SIZE)+1) //one more block to store non-perfect-mappable inodes (inode numbers that differ multiples of BD_BLOCK_SIZE)
 #else
 #define NUMBER_OF_I_MAP_BLOCKS (NUMBER_OF_INODES/BD_BLOCK_SIZE) //can map (4byte int32_t)InodeNumber -> (1byte bool)used, 512-times per I-Map Block (512Bytes)
 #endif
@@ -31,13 +30,21 @@
 #define LAST_INODE_BLOCK (FIRST_INODE_BLOCK + NUMBER_OF_INODE_BLOCKS - 1)
 
 //D-Map
+#if ((NUMBER_OF_USABLE_DATABLOCKS%BD_BLOCK_SIZE) != 0)
+#define NUMBER_OF_D_MAP_BLOCKS ((NUMBER_OF_USABLE_DATABLOCKS/BD_BLOCK_SIZE)+1) //one more block to store non-perfect-mappable values of NUMBER_OF_USABLE_DATABLOCKS
+#else
 #define NUMBER_OF_D_MAP_BLOCKS (NUMBER_OF_USABLE_DATABLOCKS/BD_BLOCK_SIZE)
+#endif
 #define D_MAP_FIRST_BLOCK (LAST_INODE_BLOCK + 1)
 #define D_MAP_LAST_BLOCK (D_MAP_FIRST_BLOCK + NUMBER_OF_D_MAP_BLOCKS - 1)
 
 //FAT
 #define FAT_SIZE_IN_BYTES (NUMBER_OF_USABLE_DATABLOCKS * 4)
+#if ((FAT_SIZE_IN_BYTES%BD_BLOCK_SIZE) != 0)
+#define NUMBER_OF_FAT_BLOCKS ((FAT_SIZE_IN_BYTES/BD_BLOCK_SIZE)+1) //one more block to store non-perfect-mappable values of NUMBER_OF_USABLE_DATABLOCKS
+#else
 #define NUMBER_OF_FAT_BLOCKS (FAT_SIZE_IN_BYTES/BD_BLOCK_SIZE)
+#endif
 #define FAT_FIRST_BLOCK (D_MAP_LAST_BLOCK + 1)
 #define FAT_LAST_BLOCK (FAT_FIRST_BLOCK + NUMBER_OF_FAT_BLOCKS - 1)
 
