2cc0ba1414e5528ccbe191e1e94b1ddd33749ddd
Tobias Sigmann <tobias.sigmann@googlemail.com>
2018-10-17 22:57:30 +0200

    create empty fs with default values

--- src/mkfs.myfs.cpp
+++ src/mkfs.myfs.cpp
@@ -10,6 +10,11 @@
 #include "macros.h"
 #include <iostream>
 #include "constants.h"
+#include "dmap.h"
+#include "fat.h"
+#include "imap.h"
+#include "rootblock.h"
+#include "superblock.h"
 
 
 
@@ -23,11 +28,43 @@ int main(int argc, char *argv[]) {
         BlockDevice* bd = new BlockDevice(BLOCK_SIZE);
         bd->create(argv[1]);
 
+        std::cout <<"1" << std::endl;
+
+        Superblock* superblock = new Superblock();
+        superblock->writeSuperblock(*bd);
+
+        std::cout <<"2" << std::endl;
+
+        DMap* dmap = new DMap();
+        dmap->writeDMap(*bd);
+
+        std::cout <<"3" << std::endl;
+
+        FatHandler* fat = new FatHandler();
+        fat->writeFat(*bd);
+
+        std::cout <<"4" << std::endl;
+
+        IMapHandler* imap = new IMapHandler();
+        imap->init();
+        imap->write(*bd);
+
+        std::cout <<"5" << std::endl;
+
+        RootBlock* rootblock = new RootBlock();
+        rootblock->init(bd);
+    
 
 
 
 
 
+        delete bd;
+        delete superblock;
+        delete dmap;
+        delete fat;
+        delete imap;
+        delete rootblock;
 
         return 0;
     }
