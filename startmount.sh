7b220977ea875b86021e8b579a3fead3228a1887
Tobias Sigmann <tobias.sigmann@googlemail.com>
2018-10-22 13:18:42 +0200

    rename folders

--- startmount.sh
+++ startmount.sh
@@ -1,7 +1,7 @@
 #bin/sh
 
 echo make files
-mkdir -p test
+mkdir -p folder1
 echo buuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu ich bin der Pape >> folder1/file1.txt
 echo buuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu ich bin der Pape >> folder1/file1.txt
 echo buuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu ich bin der Pape >> folder1/file1.txt
@@ -37,11 +37,16 @@ echo wir brauchen mehr tests >> file3.txt
 echo bild programm
 make -f Makefile
 
-echo execute
-./mkfs.myfs container.bin folder1/file1.txt file2.txt file3.txt
-
-#xxd -c 32 test.bin > hex.txt
+echo =======>execute
+./mkfs.myfs container.bin /MyFS/folder1/file1.txt /MyFS/file2.txt /MyFS/file3.txt
+echo $?
+echo =======>execute
+#xxd -c 32 container.bin > hex.txt
 
 mkdir fs
 
 ./mount.myfs container.bin log.txt fs
+
+cd fs
+ls
+cat /MyFS/log.txt
