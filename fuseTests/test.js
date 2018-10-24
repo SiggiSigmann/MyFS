d5d28551d10ff6afee830d0d43982d77cad0c326
Julian Beck <ju-fa-beck@t-online.de>
2018-10-24 22:58:52 +0200

    auto mount container.bin

--- fuseTests/test.js
+++ fuseTests/test.js
@@ -3,12 +3,22 @@ const chai = require("chai");
 const fs = require("fs");
 const path = require("path");
 const shell = require('shelljs');
+const chalk = require('chalk');
+
 let basePath ="../fs/"
 
 describe("Myfs", async function () {
     it("test if File System is mounted", function (done) {
-        
-    });
+
+        if (fs.existsSync(basePath)) {
+            expect(true).to.be.true;
+        }else{
+            console.log(chalk.blue('\t\tmounting container.bin...'));
+            shell.exec("cd .. && bash startmount.sh", {silent:true});
+            expect(fs.existsSync(basePath)).to.be.true;
+            done()
+        }
+    }).timeout(50000)
     it("getFiles", function () {
         let files = fs.readdirSync(basePath);
         expect(files).to.be.not.empty
