48ca68e6184b549ff0c40dbe705d9a1765bbe42f
Julian Beck <ju-fa-beck@t-online.de>
2018-10-25 11:32:14 +0200

    ompare container bin to testFiles

--- fuseTests/test.js
+++ fuseTests/test.js
@@ -5,24 +5,39 @@ const path = require("path");
 const shell = require('shelljs');
 const chalk = require('chalk');
 
-let basePath ="../fs/"
+let basePath = "../fs/"
+let testPath = "./testFiles"
 
 describe("Myfs", async function () {
     it("test if File System is mounted", function (done) {
 
         if (fs.existsSync(basePath)) {
             expect(true).to.be.true;
-        }else{
+            done();
+        } else {
             console.log(chalk.blue('\tmounting container.bin...'));
-            shell.exec("cd .. && bash startmount.sh", {silent:true});
+            shell.exec("cd .. && bash startmount.sh", {
+                silent: true
+            });
             expect(fs.existsSync(basePath)).to.be.true;
             done()
         }
-    }).timeout(50000)
-    it("getFiles", function () {
+    }).timeout(25000)
+    it("check if containe.bin has files", function () {
         let files = fs.readdirSync(basePath);
         expect(files).to.be.not.empty
+        //fs.writeFile('file', new Buffer(1024*1024*54));
+    });
+    describe("test reading files", function () {
+        it("Compare container bin to testFiles", function () {
+            fs.readdirSync(basePath).forEach(file => {
+                let stats = fs.statSync(path.join(basePath, file));
+                fs.readFileSync(path.join(basePath, file));
+                expect(stats.blksize).to.be.equal(512);
+                //compared container.bin files
+                expect(fs.readFileSync(path.join(basePath, file), 'utf8')).to.be.equal(fs.readFileSync(path.join(testPath, file), "utf8"));
+            });
 
+        });
     });
 });
-
