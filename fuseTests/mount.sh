3c8473412716da4c760fce9c176083f4676ab932
Julian Beck <ju-fa-beck@t-online.de>
2018-10-26 08:53:39 +0200

    moved mounting into mount.sh

---
+++ fuseTests/mount.sh
@@ -0,0 +1,5 @@
+#bin/sh
+cd ..
+make -f Makefile
+./mkfs.myfs container.bin /MyFS/fuseTests/testFiles/file1.txt /MyFS/fuseTests/testFiles/file2.txt /MyFS/fuseTests/testFiles/file3.txt /MyFS/fuseTests/testFiles/file4.txt
+./mount.myfs container.bin log.txt fs
